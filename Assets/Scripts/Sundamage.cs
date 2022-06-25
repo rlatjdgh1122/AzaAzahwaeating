f040198753b5e2640bcbec9a7cd257543d0aa0f9
rlatjdgh1122 <98933564+rlatjdgh1122@users.noreply.github.com>
2022-06-25 20:43:54 +0900

    레벨업판넬제작

--- Assets/Scripts/Sundamage.cs
+++ Assets/Scripts/Sundamage.cs
@@ -7,12 +7,18 @@ public class Sundamage : MonoBehaviour
     public float speed = 5f;
     public float tick = 0.3f;
     public float damage = 1f;
+    public float currentdamge;
+
+    private void Start()
+    {
+        currentdamge = damage;
+    }
 
     private void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Enemy"))
         {
-            collision.GetComponent<enemyTakedamage>().enemyTakedamaged(damage);
+            collision.GetComponent<enemyTakedamage>().enemyTakedamaged(currentdamge);
         }
     }
 }
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -5,9 +5,20 @@ using UnityEngine;
 public class UIManager : MonoBehaviour
 {
     [SerializeField] PlayerFire player;
+    Sundamage sun;
+
+    private void Start()
+    {
+        sun = GameObject.FindObjectOfType<Sundamage>();
+    }
     public void BulletDamageUp()
     {
-        player.CurrentbulletDamage += 1;
+        player.CurrentbulletDamage += 0.5f;
         player.closePanel();    
+    }
+    public void SunBulletDamage()
+    {
+        sun.currentdamge += 0.5f;
+        player.closePanel();
     }
 }
